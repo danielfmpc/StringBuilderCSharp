7e42549e43d60c7e2b23c3185018c978895e2b8a
Daniel Fernando <daniel_fmpc@hotmail.com.br>
2021-02-20 00:50:30 -0300

    Coments

---
+++ StringBuilderCSharp/Entities/Coment.cs
@@ -0,0 +1,16 @@
+﻿namespace StringBuilderCSharp.Entities
+{
+    class Coment
+    {
+        public string Text { get; set; }
+
+        public Coment()
+        {
+        }
+
+        public Coment(string text)
+        {
+            Text = text;
+        }
+    }
+}
