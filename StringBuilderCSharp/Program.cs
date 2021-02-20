105c5e7c2d624b9e31f3ea84d6b72fd71308405a
Daniel Fernando <daniel_fmpc@hotmail.com.br>
2021-02-20 00:48:14 -0300

    First

---
+++ StringBuilderCSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StringBuilderCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
